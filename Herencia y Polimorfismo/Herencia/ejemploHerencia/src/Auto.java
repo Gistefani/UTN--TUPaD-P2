93c0c3ed3f8b4a029e75d0d0b92cbb430c979b58
GiStefani <stefanigise@gmail.com>
2025-10-22 20:04:16 -0300

    Ejemplo de herencia

---
+++ Herencia y Polimorfismo/Herencia/ejemploHerencia/src/Auto.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Asus
+ */
+public class Auto extends Vehiculo {
+    
+    boolean tieneAire;
+    
+    void prenderAire(){
+      if (tieneAire){
+          System.out.println("Encendiendo aire..");
+      
+      }
+    
+    }
+    
+}
---
+++ Herencia y Polimorfismo/Herencia/ejemploHerencia/src/Vehiculo.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Asus
+ */
+public class Vehiculo {
+    private String marca;
+    private String Modelo;
+    private String patente;
+    
+    public void acelerar(){
+        System.out.println("Acelerando..");
+    }
+    
+    public void frenar(){
+        System.out.println("Frenando..");
+    }
+     
+    public  void encender(){
+        System.out.println("Encendiendo..");
+    }
+            
+    
+    
+}
